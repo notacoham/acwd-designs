e1a9ac15052817dbfacbc928d552bd73b29e5394
Alex Cottam <alexcottam12@gmail.com>
2024-09-28 14:38:14 -0600

    add responsive styling to Navbar, Hero, and WhatWeDo components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,12 +5,15 @@ const Hero = () => {
   return (
     <div className="hero-center">
       <div className="hero-content">
-        <h1 className="hero-title">Small Business Web Design</h1>
+        <div className="hero-title-container">
+        <h1 className="hero-title">Small Business</h1>
+        <h1 className="hero-title">Web Design</h1>
+        </div>
         <p className="hero-body">
           We Specialize in Delivering Custom, Hand Coded Websites to give your
           Business the Boost it Needs.
         </p>
-        <button className="btn">Get Started</button>
+        <button className="btn">Get A Quote Today!</button>
       </div>
       <div className="hero-img">
         <img src={heroImg} alt="Hero Img" />
