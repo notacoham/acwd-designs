08c53af1b33abf527fd3f845c57ebb30334ff974
Alex Cottam <alexcottam12@gmail.com>
2024-09-27 22:48:18 -0600

    add initial what we do files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import WhatWeDo from './components/WhatWeDo';
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <WhatWeDo />
     </>
   );
 }
---
+++ src/components/WhatWeDo.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+const WhatWeDo = () => {
+  return (
+    <div className="what-center">
+      <div className="what-content">
+        <h1 className="what-title">What We Do</h1>
+        <p className="what-body">
+          At ACWD Designs, we work with small businesses to develop websites
+          here in Millcreek, Utah. We write each line of code by hand to ensure
+          the best performance and bring more customers to your storefront. We
+          take care of the hosting and manage the edits so you have more time to
+          focus on what really matters.
+        </p>
+      </div>
+      <div className="what-list">
+        <div className="what-item">
+          <img src="" alt="" className="what-img" />
+          <h2 className="what-item-title">Responsive Design</h2>
+          <p className="what-item-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero eius
+            qui recusandae cumque eligendi magni impedit. Nam unde alias natus
+            fuga eum a ipsam, eius cupiditate, ex sunt minus officiis?
+          </p>
+          <a className="small-btn">Get Started</a>
+        </div>
+        <div className="what-item">
+          <img src="" alt="" className="what-img" />
+          <h2 className="what-item-title">Support Local</h2>
+          <p className="what-item-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero eius
+            qui recusandae cumque eligendi magni impedit. Nam unde alias natus
+            fuga eum a ipsam, eius cupiditate, ex sunt minus officiis?
+          </p>
+          <a className="small-btn">Get Started</a>
+        </div>
+        <div className="what-item">
+          <img src="" alt="" className="what-img" />
+          <h2 className="what-item-title">Boost your Visibility</h2>
+          <p className="what-item-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero eius
+            qui recusandae cumque eligendi magni impedit. Nam unde alias natus
+            fuga eum a ipsam, eius cupiditate, ex sunt minus officiis?
+          </p>
+          <a className="small-btn">Get Started</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WhatWeDo;
