d63ea6f228ae461b1fa2d5ae06f32edf1b4768eb
Alex Cottam <alexcottam12@gmail.com>
2024-09-28 16:20:01 -0600

    add icons to what we do cards

--- src/components/WhatWeDo.jsx
+++ src/components/WhatWeDo.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import icon1 from '../assets/icons/icons8-smartphone-96.png'
+import icon2 from '../assets/icons/icons8-utah-96.png'
+import icon3 from '../assets/icons/icons8-search-engine-optimization-96.png'
 
 const WhatWeDo = () => {
   return (
@@ -15,34 +18,29 @@ const WhatWeDo = () => {
       </div>
       <div className="what-list">
         <div className="what-item">
-          <img src="" alt="" className="what-img" />
+          <img src={icon1} alt="" className="what-img" />
           <h2 className="what-item-title">Responsive Design</h2>
           <p className="what-item-body">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero eius
-            qui recusandae cumque eligendi magni impedit. Nam unde alias natus
-            fuga eum a ipsam, eius cupiditate, ex sunt minus officiis?
+            We make sure to build your site with all sizes of screens in mind so
+            visitors can access your site from anywhere and see your beautiful website no matter what device they're using.
           </p>
-          <a className="small-btn">Get Started</a>
+          <button className="what-btn">Get Started</button>
         </div>
         <div className="what-item">
-          <img src="" alt="" className="what-img" />
+          <img src={icon2} alt="" className="what-img" />
           <h2 className="what-item-title">Support Local</h2>
           <p className="what-item-body">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero eius
-            qui recusandae cumque eligendi magni impedit. Nam unde alias natus
-            fuga eum a ipsam, eius cupiditate, ex sunt minus officiis?
+            ACWD Designs is a local web development company based in Millcreek, Utah. We're committed to helping businesses in our community thrive by making professional website development accessible to all, regardless of size or budget
           </p>
-          <a className="small-btn">Get Started</a>
+          <button className="what-btn">Get Started</button>
         </div>
         <div className="what-item">
-          <img src="" alt="" className="what-img" />
-          <h2 className="what-item-title">Boost your Visibility</h2>
+          <img src={icon3} alt="" className="what-img" />
+          <h2 className="what-item-title">Boost your Visibility w/ SEO</h2>
           <p className="what-item-body">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero eius
-            qui recusandae cumque eligendi magni impedit. Nam unde alias natus
-            fuga eum a ipsam, eius cupiditate, ex sunt minus officiis?
+            Supercharge your local presence with our SEO expertise. We'll optimize your website to attract more eyes, climb search rankings, and turn online visibility into real-world customers. From Millcreek to beyond, we'll put your business on the digital map and drive more traffic - both online and through your door.
           </p>
-          <a className="small-btn">Get Started</a>
+          <button className="what-btn">Get Started</button>
         </div>
       </div>
     </div>
