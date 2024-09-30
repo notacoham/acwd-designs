cf103ac83762f9aef974d5d4f7359d50b3d65100
Alex Cottam <alexcottam12@gmail.com>
2024-09-30 17:25:24 -0600

    add about page initial files

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import profileImg from '../assets/alex-cottam-profile-img-edited.png'
+import WaveBottom from '../components/WaveBottom'
+import { NavLink } from 'react-router-dom'
+import Pricing from '../components/Pricing'
 
 const About = () => {
   return (
-    <div>
-      About
+    <div className='about-center'>
+      <div className="about-head-container">
+        <h1 className="about-head-title">About Us</h1>
+        <WaveBottom />
+      </div>
+      <div className="about-me-center">
+        <img src={profileImg} alt="" className="about-me-img" />
+        <div className="about-me-content">
+          <h2 className="about-me-subtitle">ACWD Designs</h2>
+          <h1 className="about-me-title">A Passion For Design and Small Business</h1>
+          <p className="about-me-body">My name is Alex, owner and lead developer of ACWD Designs. My passion for technology and art naturally led me to web development, where I found the perfect blend of creativity and technical skill.
+            Throughout my life, I've cherished the unique charm of small businesses - those hidden gems that make our communities special. However, I noticed a troubling trend: many of these businesses struggle to afford professional, functional websites that truly showcase their value.
+          </p>
+          <p className="about-me-body">That's why I created ACWD Designs. Our mission is to provide small businesses with stunning, effective websites at prices they can afford. Starting at just $1,200, we're making professional web design accessible to all.
+            At ACWD Designs, we're not just building websites - we're helping small businesses shine online, one pixel at a time.
+          </p>
+          <NavLink to='/contact' className="price-btn">Get Started</NavLink>
+        </div>
+      </div>
+      <Pricing />
     </div>
   )
 }
